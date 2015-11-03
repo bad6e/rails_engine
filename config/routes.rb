ab3bc28e8c19238a69026f6e5ac33ccd0a385b8b
Bret Doucette <doucette.bret@gmail.com>
2015-11-02 18:45:22 -0700

    Implemented index apis

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
  namespace :api do
     namespace :v1 do
-      resources :merchants, except: [:new, :edit], defaults: {format: :json}
+      resources :customers, only: [:index, :show], defaults: {format: :json}
+      resources :merchants, only: [:index, :show], defaults: {format: :json}
+      resources :items, only: [:index, :show], defaults: {format: :json}
+      resources :invoice_items, only: [:index, :show], defaults: {format: :json}
+      resources :transactions, only: [:index, :show], defaults: {format: :json}
+      resources :invoices, only: [:index, :show], defaults: {format: :json}
     end
   end
 end
