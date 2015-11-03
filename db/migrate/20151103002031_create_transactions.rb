dd1d5b12c9d3142af871068d523d7fbd10c584ef
Bret Doucette <doucette.bret@gmail.com>
2015-11-02 21:36:57 -0700

    Added in citext columns for case insensitivity

--- db/migrate/20151103002031_create_transactions.rb
+++ db/migrate/20151103002031_create_transactions.rb
@@ -1,10 +1,12 @@
 class CreateTransactions < ActiveRecord::Migration
   def change
+    enable_extension 'citext'
+
     create_table :transactions do |t|
       t.references :invoice, index: true, foreign_key: true
-      t.string :cc_number
+      t.citext :cc_number
       t.date :cc_exp_date
-      t.string :result
+      t.citext :result
 
       t.timestamps null: false
     end
