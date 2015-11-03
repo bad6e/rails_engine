84d690ed5357d795d46f4bc20b0dc8fceef83f30
Bret Doucette <doucette.bret@gmail.com>
2015-11-02 21:00:58 -0700

    Added single finder for attributes of all models

--- app/controllers/api/v1/invoice_items_controller.rb
+++ app/controllers/api/v1/invoice_items_controller.rb
@@ -5,7 +5,17 @@ respond_to :json
     respond_with InvoiceItem.all
   end
 
-   def show
+  def show
     respond_with InvoiceItem.find_by(id: params[:id])
+  end
+
+  def find
+    respond_with InvoiceItem.find_by(invoice_items_params)
+  end
+
+  private
+
+  def invoice_items_params
+    params.permit(:id, :item_id, :invoice_id, :quantity, :unit_price, :created_at, :updated_at)
   end
 end
--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -5,7 +5,17 @@ class Api::V1::ItemsController < ApplicationController
     respond_with Item.all
   end
 
-   def show
+  def show
     respond_with Item.find_by(id: params[:id])
+  end
+
+  def find
+    respond_with Item.find_by(item_params)
+  end
+
+  private
+
+  def item_params
+    params.permit(:id, :name, :description, :unit_price, :merchant_id, :created_at, :updated_at)
   end
 end
