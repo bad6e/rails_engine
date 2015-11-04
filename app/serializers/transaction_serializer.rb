81635a74314a448be020fe80e5c6c7373c7d62a9
Bret Doucette <doucette.bret@gmail.com>
2015-11-03 21:53:14 -0700

    Made string to i

--- app/serializers/transaction_serializer.rb
+++ app/serializers/transaction_serializer.rb
@@ -1,4 +1,4 @@
 class TransactionSerializer < ActiveModel::Serializer
-  attributes :id, :created_at, :credit_card_number, :invoice_id, :result, :updated_at
+  attributes :id, :created_at, :credit_card_number, :invoice_id, :result, :updated_at, :created_at
   belongs_to :invoice_id
 end
